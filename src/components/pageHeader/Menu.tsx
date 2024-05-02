343e52929664ef6f7de7f00545011b60731d5018
Robert Scott <rsrob600@gmail.com>
2024-05-02 16:09:39 +0000

    sign-in

--- src/components/pageHeader/Menu.tsx
+++ src/components/pageHeader/Menu.tsx
@@ -1,13 +1,37 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import {
+  LoginContext,
+  LoginContextType,
+  UserType,
+} from "../../context/context";
 
+const Menu = (): JSX.Element => {
+  // if theuser is logged in, render their name
+  // if not, then render sign in button
 
-const Menu = () : JSX.Element => {
-    return (
-        <ul className="nav">
-            <li><Link to="/find">Find a transaction</Link></li>
-            <li><Link to="/add">New transaction</Link></li>
-        </ul>
-    );
-}
+  // make it so that
+  //if the user is logged in, render their name
+  // if they are not logged in, render the login link
+
+  const contextPointer = useContext<LoginContextType>(LoginContext);
+
+  const userDetails: UserType = contextPointer.user;
+
+  return (
+    <ul className="nav">
+      <li>
+        <Link to="/find">Find a transaction</Link>
+      </li>
+      <li>
+        <Link to="/add">New transaction</Link>
+      </li>
+      <li>
+        {" "}
+        <Link to="/signin">Sign In</Link>
+      </li>
+    </ul>
+  );
+};
 
 export default Menu;
